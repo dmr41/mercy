2a5a5c405f152ec6a73d7001a4c294dc8c12a995
David Rivers <dmr41.rivers@gmail.com>
2015-03-04 16:28:27 -0700

    Add gravatar functionality to page

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,10 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
   actions: {
     newPath: function() {
       this.transitionToRoute('rants.new');
     },
+
     signupPath: function() {
       this.transitionToRoute('users.new');
     },
