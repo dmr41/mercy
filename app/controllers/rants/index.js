acab63f4a701cfc0f06309e1a004b6d8611d416a
David Rivers <dmr41.rivers@gmail.com>
2015-03-05 10:15:39 -0700

    adding loading.hbs to templates - spinner added

--- app/controllers/rants/index.js
+++ app/controllers/rants/index.js
@@ -1,6 +1,5 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
-  stuff: true,
-
+export default Ember.ObjectController.extend({
+  stuff: localStorage.getItem('stuff'),
 });
--- app/routes/rants.js
+++ app/routes/rants.js
@@ -1,8 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
+  stuff: localStorage.setItem('stuff', false),
   model: function(){
-    return this.store.find('rant')
-  }
+    return this.store.find('rant').then(function(rants){
+      var stuff = localStorage.getItem('stuff');
+      stuff = localStorage.setItem('stuff', false);
+      return rants;
+    });
+  },
+
+
+
+
+
 });
