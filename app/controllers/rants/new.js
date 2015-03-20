688990236e4c3df47afea2f893a39b8a7b5cdede
David Rivers <dmr41.rivers@gmail.com>
2015-03-19 20:28:59 -0600

    working version

--- app/controllers/rants/new.js
+++ app/controllers/rants/new.js
@@ -11,16 +11,7 @@ export default Ember.ObjectController.extend({
     editCancel: function() {
       this.set('isEditing', true);
     },
-    // editSave: function(rant) {
-    //   var body = rant.get('body');
-    //   var title = rant.get('title');
-    //   var user  = this.session.id;
-    //   if(body && title) {
-    //     this.set('isEditing', true);
-    //     rant.save();
-    //     this.transitionToRoute('rants');
-    //   }
-    // },
+  
     newRant: function () {
     var user = this.get('session.email'),
         body = this.get('body'),
@@ -28,9 +19,9 @@ export default Ember.ObjectController.extend({
         self = this;
 
     this.store.findQuery('user', {email: user}).then(function(currentRanter) {
-      var current = currentRanter.get('firstObject')
+      var current = currentRanter.get('firstObject');
       if (body && title) {
-        var rant = self.store.createRecord('rant', { body: body, title: title, user: current, });
+        var rant = self.store.createRecord('rant', { body: body, title: title, user: current });
         rant.save();
       }
     });
