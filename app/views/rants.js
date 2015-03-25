7265b617b72fc54309b15245def6c970fe0aad8b
David Rivers <dmr41.rivers@gmail.com>
2015-03-25 06:52:30 -0600

    changed users personal rant page format

--- app/views/rants.js
+++ app/views/rants.js
@@ -7,6 +7,9 @@ export default Ember.View.extend({
              this.$(".rant-anima").hide();
 
              $('.rant-anima').fadeIn(1000);
+             $('.rant-anima').append('<h1>HIHIHIHIHIH</h1>');
+             $('.input-format').append('<i class="fa fa-save"></i>');
+
 
 
      }
